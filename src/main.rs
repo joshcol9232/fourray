e475321206daab5f714280888e416828bddd200e
Josh Colclough <colcloughj2@cardiff.ac.uk>
2022-05-22 06:17:33 +0100

    Raymarching -> Raytracing

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,31 @@
 use nalgebra as na;
 
-use std::io::Write;
-use na::{Point4, Vector4, Vector3};
+use std::{io::Write, mem::Discriminant};
+use std::boxed::Box;
+use na::{Vector4, Vector3, Normed};
 use image::{DynamicImage, GenericImage, Rgba};
 
-const DIMS: [u32; 2] = [800, 800];
+const DIMS: [u32; 2] = [1200, 1200];
 
 pub type Colour = Vector3<f64>;
 
 
+struct RayHit {
+    pos: Vector4<f64>,
+    colour: Colour,
+    shade: f64,
+}
+
+
 struct Ray {
-    orig: Point4<f64>,
+    orig: Vector4<f64>,
     dir: Vector4<f64>,
     pixel: [u32; 2],
     colour: Colour,
 }
 
 impl Ray {
-    pub fn new(o: Point4<f64>, d: Vector4<f64>, i: u32, j: u32) -> Self {
+    pub fn new(o: Vector4<f64>, d: Vector4<f64>, i: u32, j: u32) -> Self {
         Self {
             orig: o,
             dir: d,
@@ -26,63 +34,115 @@ impl Ray {
         }
     }
 
-    pub fn march(&self, dt: f64, objects: &[Sphere], nearplane: f64, farplane: f64) -> Option<RayHit> {
-        // March ray forwards in increments of dt, from nearplane up to farplane
-        let mut t: f64 = nearplane;
-        let mut raypos: Point4<f64> = self.orig.clone();
+    #[inline]
+    fn at(&self, t: f64) -> Vector4<f64> {      // Ray equation
+        self.orig + t * self.dir
+    }
 
-        // List of hits found (all objects must be checked, the nearest in `t` is correct hit).
-        let mut current_hits = vec![0.0; objects.len()];
+    pub fn trace(&self, objects: &[Box<dyn Object>]) -> Option<RayHit> {
+        for (i, obj) in objects.iter().enumerate() {
+            let intersect = obj.ray_intersection(self); // Returns depth at intersection
+            // println!("Intersect: {}", intersect);
+            if intersect > -1.0 {  // Has hit
+                println!("Doing raypos stuff.");
+                let raypos = self.at(intersect);
 
-        while t < farplane {
-            for (i, obj) in objects.iter().enumerate() {
+                // APPLY BASIC SHADING
+                // TODO: Make shading better. Just apply some shading from far away on +ve all axis for now
+                let mut norm_line_to_light = Vector4::new(100.0, -100.0, -50.0, 0.0) - raypos;
+                norm_line_to_light /= norm_line_to_light.norm();
 
-                if obj.is_point_inside(&raypos) {
-                    return Some(RayHit {        // Break as an object has been hit
-                        pos: raypos.clone(),
-                        colour: self.colour.component_mul(&obj.colour),
-                    })
-                }
+
+                let shade = obj
+                    .surface_normal(&raypos)
+                    .dot(&norm_line_to_light)
+                    .max(0.0);
+                    
+                // println!("Shade: {}", shade);
+
+                return Some(RayHit {        // Break as an object has been hit
+                    pos: raypos.clone(),
+                    colour: self.colour.component_mul(obj.colour()),
+                    shade,
+                })
             }
-            // Ray defined by P(t) = origin + t * dir
-            raypos += self.dir * dt;
-            t += dt;
         }
 
         None
     }
 }
 
-struct RayHit {
-    pos: Point4<f64>,
-    colour: Colour,
+
+trait Object {
+    fn ray_intersection(&self, ray: &Ray) -> f64;
+    fn colour(&self) -> &Colour;
+    fn surface_normal(&self, p: &Vector4<f64>) -> Vector4<f64>;
 }
 
 struct Sphere {
-    pos: Point4<f64>,
+    pos: Vector4<f64>,
     rad: f64,
     colour: Colour,
 }
 
 impl Sphere {
-    pub fn new(pos: Point4<f64>, rad: f64, colour: Colour) -> Self {
+    pub fn new(pos: Vector4<f64>, rad: f64, colour: Colour) -> Self {
         Self {
             pos,
             rad,
             colour,
         }
     }
+}
+impl Object for Sphere {
+    fn ray_intersection(&self, ray: &Ray) -> f64 {
+        // https://raytracing.github.io/books/RayTracingInOneWeekend.html#overview
+        let diff = ray.orig - self.pos;
+        let b = 2.0 * ray.dir.dot(&diff);
+        let c = diff.norm_squared() - self.rad.powi(2);
+        // a = 1 because a is the dot product of the ray direction with itself, so 1 squared.
 
-    pub fn is_point_inside(&self, p: &Point4<f64>) -> bool {
-        (p - self.pos).norm_squared() < self.rad.powi(2)
+        let discriminant = b.powi(2) - 4.0 * c;
+        if discriminant >= 0.0 {
+            // Now find location of hit
+            println!("Hit!: {}", discriminant);
+            (-b - discriminant.sqrt() ) / 2.0
+        } else {
+            -1.0
+        }
     }
 
-    pub fn surface_normal(&self, p: &Point4<f64>) -> Vector4<f64> {
+    fn surface_normal(&self, p: &Vector4<f64>) -> Vector4<f64> {
         let mut v = p - self.pos;
         v /= v.norm();
         v
     }
+
+    fn colour(&self) -> &Colour {
+        &self.colour
+    }
 }
+
+/*
+struct Floor {
+    y: f64,
+    colour: Colour,
+}
+
+impl Object for Floor {
+    fn is_point_inside(&self, p: &Vector4<f64>) -> bool {
+        p[1] > self.y       // Below floor (+ve y is down)
+    }
+
+    fn surface_normal(&self, p: &Vector4<f64>) -> Vector4<f64> {
+        Vector4::new(0.0, 1.0, 0.0, 0.0)
+    }
+
+    fn colour(&self) -> &Colour {
+        &self.colour
+    }
+}
+*/
 
 /* 
 fn cross4D(a: &Vector4<f64>, b: &Vector4<f64>, c: &Vector4<f64>) -> Vector4<f64> {
@@ -106,7 +166,7 @@ fn get_rgba(c: Colour) -> Rgba<u8> {
     ])
 }
 
-fn spawn_rays(origin: Point4<f64>, viewdir: Vector4<f64>, upvec: Vector4<f64>, limbovec: Vector4<f64>, fov: f64, xmax: u32, ymax: u32) -> Vec<Ray> {
+fn spawn_rays(origin: Vector4<f64>, viewdir: Vector4<f64>, upvec: Vector4<f64>, limbovec: Vector4<f64>, fov: f64, xmax: u32, ymax: u32) -> Vec<Ray> {
     /* Idea: Viewport is plane of dims 1 x 1
     Scan along viewport in dx = 1/X, dy = 1/Y, starting at x = -1/2, y = -1/2
     Offset viewport from origin by some distance `d`.
@@ -142,12 +202,11 @@ fn spawn_rays(origin: Point4<f64>, viewdir: Vector4<f64>, upvec: Vector4<f64>, l
             o[0] += x;
             o[1] += y;
 
-            let mut d = (o - origin);
+            let mut d = o - origin;
             d /= d.norm();
             let r = Ray::new(o, d, i, j);
 
             rays.push(r);
-            // println!("{} {}", i, j);
 
             x += dx;
             i += 1;
@@ -164,7 +223,7 @@ fn main() {
     let mut img = DynamicImage::new_rgb8(DIMS[0], DIMS[1]);
 
     let rays = spawn_rays(
-        Point4::new(0.0, 0.0, 0.0, 0.0),
+        Vector4::new(0.0, 0.0, 0.0, 0.0),
         Vector4::new(0.0, 0.0, 1.0, 0.0),  // NOTE: Doesn't make any difference atm
         Vector4::new(0.0, 1.0, 0.0, 0.0),
         Vector4::new(0.0, 0.0, 0.0, 1.0),  // Effectively makes it 3D
@@ -173,15 +232,21 @@ fn main() {
         DIMS[1],
     );
 
-    let objects = vec![
-        Sphere::new(Point4::new(0.0, 0.0, 2.0, 0.0), 1.0, Colour::new(0.0, 1.0, 0.0)),
+    let objects: Vec<Box<dyn Object>> = vec![
+        Box::new(Sphere::new(Vector4::new(0.0, 0.0, 4.0, 0.0), 3.0, Colour::new(0.0, 1.0, 0.0))),
+        Box::new(Sphere::new(Vector4::new(1.0, 0.0, 4.0, 0.0), 3.0, Colour::new(1.0, 0.0, 0.0))),
+        // Box::new(Floor { y: 1.5, colour: Colour::new(1.0, 0.0, 1.0) }),
     ];
 
+
     for (i, ray) in rays.iter().enumerate() {
-        // println!("Marching ray {}/{}.", i, rays.len());
-        let hitcolour: Colour = match ray.march(0.01, &objects, 1.0, 10.0) {
-            Some(hit) => hit.colour,
-            None => Colour::new(0.0, 0.0, 0.0),
+        if i as u32 % DIMS[1] == 0 {
+            println!("Done {}/{} scanlines.", i as u32/DIMS[1], DIMS[1]);
+        }
+        
+        let hitcolour: Colour = match ray.trace(&objects) {
+            Some(hit) => hit.colour * hit.shade,
+            None => Colour::new(0.8, 0.8, 0.8),
         };
 
         img.put_pixel(ray.pixel[0], ray.pixel[1], get_rgba(hitcolour));
